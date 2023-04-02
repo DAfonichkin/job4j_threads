72aebabdb60e08c05417fe89e3aa4dc34312794e
Dmitriy Afonichkin <dafonichkin@gmail.com>
2023-04-03 01:25:41 +0300

    2. ExecutorService рассылка почты. [#63097 #406329]

--- src/main/java/ru/job4j/email/EmailNotification.java
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -5,11 +5,7 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    private final ExecutorService pool;
-
-    public EmailNotification(ExecutorService pool) {
-        this.pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    }
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
@@ -19,6 +15,13 @@ public class EmailNotification {
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) { }
